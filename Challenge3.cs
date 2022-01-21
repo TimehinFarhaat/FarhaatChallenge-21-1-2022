88b983a6c23cc290fffb421bdc893306d57bd508
TimehinFarhaat <timehinfarhaat@gmail.com>
2022-01-21 12:46:21 +0100

    FarhaatCodeChallenge

--- Challenge3.cs
+++ Challenge3.cs
@@ -8,58 +8,7 @@ namespace Code
 {
     class Challenge3
     {
-      public static void Contigious()
-      {
-          int[] sum = new[] {5};
-          int k = 1;
-          int so=0;
-          float l = 0;
-          for (int i = 0; i <= sum.Length - k; i++)
-          {
-              so = 0;
-              for (int j = i; j < k + i; j++)
-              {
-                  so += sum[j];
 
-              }
-
-              if (so > l )
-              {
-                  l = so ;
-              }
-          }
-          Console.WriteLine((l / k).ToString("0.00000"));
-          
-      }
-
-
-      public static void Palindrome()
-      {
-          string s = "abca".ToLower();
-          char[] f = s.ToCharArray();
-          string h = "";
-          string c = "";
-          for (int i = f.Length - 1, j = 0; i >= f.Length / 2 && j <= i; i--, j++)
-          {
-              if (f[i]==f[j])
-              {
-                  c += f[i] + f[j];
-              } 
-              if (f[i]!=f[j])
-              {
-                  h += f[j];
-              }
-          }
-          if (c.Length != 0 && (h.Length==2 ||h.Length==1 ||h.Length==0))
-          {
-              Console.WriteLine("True");
-          }
-          else
-          {
-              Console.WriteLine("False");
-          }
-          
-
-      }
     }
 }
+  
--- Program.cs
+++ Program.cs
@@ -1,37 +1,128 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+
 
 namespace Code
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-           // Indices();
-           //Challenge2. PerfectNumber();
-            Challenge3.Contigious();
-          // Challenge3.Palindrome();
+            Question2();
+            Question1();
         }
 
 
-        static void Indices()
+        public static void Question2()
         {
-            int[] d = new[] { 3,1,0,11 };
-            bool s = false;
-            for (int i = 0; i < d.Length; i++)
+            int[] a = new[] { 1, 3, 2, 2, 5, 2, 3, 7 };
+            HashSet<int> b = new HashSet<int>();
+            for (int i = 0; i < a.Length; i++)
             {
-                for (int j = 0; j < d.Length; j++)
+                for (int j = 0; j < a.Length - 1; j++)
                 {
-                    if (d[i] != d[j])
+                    if (a[i] > a[j])
                     {
-                        if (d[i] * 2 == d[j])
+                        if (a[i] - a[j] == 1)
                         {
-                            s = true;
-                            break;
+                            b.Add(a[i]);
+                            b.Add(a[j]);
+
+                        }
+                    }
+                    else
+                    {
+                        if (a[j] - a[i] == 1)
+                        {
+                            b.Add(a[i]);
+                            b.Add(a[j]);
                         }
                     }
                 }
             }
-            Console.WriteLine(s);
+
+            Hashtable t = new Hashtable();
+            foreach (var c in b)
+            {
+                t.Add(c, 0);
+            }
+
+            foreach (var r in a)
+            {
+                if (t.ContainsKey(r))
+                {
+                    t[r] = (int) t[r] + 1;
+                }
+            }
+
+            List<int> list = new List<int>();
+            List<int> list1 = new List<int>();
+            foreach (DictionaryEntry entry in t)
+            {
+                list.Add((int) entry.Key);
+                list1.Add((int) entry.Value);
+            }
+
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                for (int j = 0; j < list.Count - 1; j++)
+                {
+                    if (list[j] > list[j + 1])
+                    {
+                        int temp = list[j];
+                        list[j] = list[j + 1];
+                        list[j           + 1] = temp;
+
+                        int tem = list1[j];
+                        list1[j] = list1[j + 1];
+                        list1[j            + 1] = tem;
+
+
+                    }
+                }
+            }
+
+            int w = 0;
+            int y = 0;
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                if (list[i + 1] - list[i] == 1)
+                {
+                    w = list1[i] + list1[i + 1];
+                    if (y < w)
+                    {
+                        y = w;
+                    }
+
+                }
+            }
+
+            Console.WriteLine(y);
+
+
+        }
+
+
+        public static void Question1()
+        {
+            int a = 5;
+            int v = 1;
+            for (int i = 1; i <= a; i++)
+            {
+                a -= v;
+                v++;
+            }
+            Console.WriteLine(v-1);
+
+
         }
     }
+
+
+
+
+
 }
+     
